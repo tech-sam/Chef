b4f93b2fdd5e021bd1477fa3097fe2d5bb4dde72
tech-sam <sumit.s7325@gmail.com>
2021-03-22 08:00:41 +0530

    ecto timestamp changes and save data

--- lib/chef/match_data.ex
+++ lib/chef/match_data.ex
@@ -1,11 +1,19 @@
 defmodule Chef.MatchData do
   use Ecto.Schema
+  @timestamps_opts [type: :utc_datetime]
 
   schema "match_data" do
-    field :home_team, :string
-    field :away_team, :string
-    field :provider, :string
-    field :kickoff_at, :utc_datetime
-    field :created_at, :utc_datetime
+    field(:home_team, :string)
+    field(:away_team, :string)
+    field(:provider, :string)
+    field(:kickoff_at, :utc_datetime)
+    field(:created_at, :utc_datetime)
+    timestamps()
+  end
+
+  def changeset(match_data, params \\ %{}) do
+    match_data
+    |> Ecto.Changeset.cast(params, [:home_team, :away_team, :provider, :kickoff_at, :created_at])
+    |> Ecto.Changeset.validate_required([:home_team, :away_team, :provider, :kickoff_at, :created_at])
   end
 end
--- lib/provider/data_provider.ex
+++ lib/provider/data_provider.ex
@@ -52,16 +52,46 @@
 # end
 
 defmodule Chef.DataProvider do
-
   alias Chef.HttpClient, as: HttpClient
+
   def start_data_fetch do
     IO.puts("start data fecting")
-    data = HttpClient.get("http://forzaassignment.forzafootball.com:8080/feed/matchbeam")
-    IO.puts data
+    # data = HttpClient.get("http://forzaassignment.forzafootball.com:8080/feed/matchbeam")
+    # IO.puts data
+    # {
+    #   "created_at": 1616337058,
+    #   "kickoff_at": "2021-04-19T14:00:00Z",
+    #   "teams": "Sporting Gijon - Real Zaragoza"
+    # }
+
+    {:ok, created_at} = DateTime.from_unix(1_616_337_058)
+
+    {:ok, kickoff_at, 0} = DateTime.from_iso8601("2021-04-19T14:00:00Z")
+
+    match = %Chef.MatchData{
+      home_team: "AFC Bournemouth",
+      away_team: "Real Zaragoza",
+      provider: "matchbeam",
+      created_at: created_at,
+      kickoff_at: kickoff_at
+    }
+     changeset = Chef.MatchData.changeset(match, %{})
+    # {:ok, test} = Chef.Repo.insert(changeset)
+    # IO.inspect(test)
+   # IO.puts("finish data processing")
+
+    case Chef.Repo.insert(changeset) do
+      {:ok, match} ->
+        # do something with person
+        IO.inspect(match)
+      {:error, yoyo} ->
+        IO.inspect(yoyo.errors)
+    end
+    IO.puts("finish data processing")
+
     # path = :code.lib_dir(:elixir, :ebin)
     # mods = Protocol.extract_impls(:MatchDataProvider, [path])
     # IO.inspect(mods)
-
   end
 end
 
